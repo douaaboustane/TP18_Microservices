c6dd9e05adad130761a198a1a047ffff46317f35
Douaa BOUSTANE <156180872+douaaboustane@users.noreply.github.com>
2026-01-03 22:04:05 +0100

    Delete Desktop directory

--- Desktop/TP18/src/main/java/ma/projet/grpc/repositories/CompteRepository.java
+++
@@ -1,8 +0,0 @@
-package ma.projet.grpc.repositories;
-
-import ma.projet.grpc.entities.Compte;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompteRepository extends JpaRepository<Compte, String> {
-}
-
